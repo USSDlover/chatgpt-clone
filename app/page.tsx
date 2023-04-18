61d807cb59491989a4357c4b1e0309da7e12e523
Alireza <a.razinejad@gmail.com>
2023-04-18 19:15:56 +0330

    [Feature] Complete the auth solution

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import { SunIcon, BoltIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
 function HomePage() {
-    console.log('session');
     return (
         <div className={'flex flex-col items-center justify-center h-screen px-2 text-white'}>
             <h1 className={'text-5xl font-bold mb-20'}>ChatGPT</h1>
