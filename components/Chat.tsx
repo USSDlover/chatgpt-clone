09b6998d01c40c0fcbdea99a74027abb8b22b222
Alireza <a.razinejad@gmail.com>
2023-04-20 20:35:13 +0430

    [Feature] Render chat messages

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,10 +1,31 @@
+'use client'
+
+import { useSession } from 'next-auth/react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { orderBy, query } from '@firebase/firestore';
+import { collection } from 'firebase/firestore';
+import { db } from '@/firebase';
+import Message from '@/components/Message';
+
 type Props = {
     chatId: string;
 }
 
 function Chat({ chatId }: Props) {
+    const { data: session } = useSession();
+
+    const [ messages ] = useCollection(session && query(
+        collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+        orderBy('createdAt', 'asc')
+    ));
+
     return (
-        <div className={'flex-1'}>Chat</div>
+        <div className={'flex-1'}>
+            { messages?.docs && messages.docs.map((message) => (
+                <Message key={message.id} message={message.data()} />
+            )) }
+            <></>
+        </div>
     );
 }
 
